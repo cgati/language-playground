960af5b8edbd97c114d8d9f27af31621e86b8f8d
Jared Ashcraft <jaredmashcraft@gmail.com>
2013-08-13 00:24:27 -0400

    Interesting (but supremely annoying) aspect of Haskell involving hard tabs vs. soft tabs. In short, Haskell doesn't treat hard tabs as actual indentation, which is important because Haskell has a Python-esque approach to statement blocks. So, always use regular spaces, or set up vim so that it only uses soft tabs.

---
+++ haskell/jared/tabExample.hs
@@ -0,0 +1,11 @@
+tabTest :: String
+tabTest = "This will work because " ++ filler ++ reasonItWorks
+  where filler = ""
+        reasonItWorks = "I used spaces instead of tabs to indent this."
+
+
+tabTestFailure :: String
+tabTestFailure = "This will not work because " ++ reasonItFails
+	where filler = ""
+				reasonItFails = "I used tabs instead of spaces to indent this."
+
